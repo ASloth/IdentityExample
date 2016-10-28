3a0f0a0c828b7e1ce28c9b4ac90c36ceef3f8eb6
Konrad Mueller <i15019@hb.dhbw-stuttgart.de>
2016-10-28 15:51:41 +0200

    Projektdateien hinzufügen.

---
+++ src/App2Night.AuthenticationExample/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App2Night.AuthenticationExample.Controllers
+{
+    [Authorize] //-> Endpunkte benötigen Authentifizierung
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        //Öffentliche Werte die ohne Authentifizierung abgerufen werden können.
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+         
+        [HttpGet("{id}")]
+        public string Get(int id)
+        { 
+            var caller = User as ClaimsPrincipal;
+            var userInformation = caller.Claims.FirstOrDefault(o => o.Type == "name");
+
+            var result = $"Hello  {userInformation} your super super secret is: secret, wow.";
+
+
+            foreach (Claim claim in caller.Claims)
+            {
+                result += "\n" + claim.Type + " " + claim.Value;
+            }
+            return result;
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/App2Night.IdentityServer/config.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Services.InMemory;
+
+namespace App2Night.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Scope> GetScopes()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API",
+                    IncludeAllClaimsForUser = true,
+                },
+                new Scope()
+                {
+                    Name = "identity",
+Type = ScopeType.Resource,
+
+    Claims = new List<ScopeClaim>
+        {
+              new ScopeClaim("role"),
+              new ScopeClaim(ClaimTypes.Name),
+              new ScopeClaim(ClaimTypes.GivenName)
+        },
+
+    IncludeAllClaimsForUser = true
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "nativeApp",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = {"api1", "identity", StandardScopes.Profile.Name, StandardScopes.OpenId.Name, "openid"}
+                }
+            };
+        }
+
+
+        public static List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Subject = "1",
+                    Username = "alice",
+                    Password = "password" 
+                },
+                new InMemoryUser
+                {
+                    Subject = "2",
+                    Username = "bob",
+                    Password = "password",
+                    Claims = new []
+                    {
+                        new Claim(ClaimTypes.Name, "Bobss"), 
+                        new Claim(ClaimTypes.GivenName, "Testname")
+                    }
+                }
+            };
+        }
+    }
+}
